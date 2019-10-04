79b202efb686d3c34b93a8832c7c09235f4f57ff
tywalker <tywalker_@outlook.com>
2019-10-04 22:34:10 +0000

    vim is a bitch

--- leet/reverse-int.c
+++ leet/reverse-int.c
@@ -6,25 +6,25 @@ int
 reverse(long myInt)
 {
     bool isNeg = myInt < 0;
-	int reverse = 0;
+    int reverse = 0;
     int toAdd = 0;
 
-	if (isNeg) 
-		myInt = -myInt;
+    if (isNeg)
+        myInt = -myInt;
 
-	while (myInt != 0)
-	{
-		toAdd = myInt % 10;
-		if (reverse > INT_MAX/10 || (reverse == INT_MAX / 10 && toAdd > 7))
-			return 0;
-		if (reverse < INT_MIN/10 || (reverse == INT_MIN / 10 && toAdd < -8))
-			return 0;
-		reverse = (reverse * 10) + toAdd;
-		myInt = myInt / 10;
-	}
+    while (myInt != 0)
+    {
+        toAdd = myInt % 10;
+        if (reverse > INT_MAX/10 || (reverse == INT_MAX / 10 && toAdd > 7))
+            return 0;
+        if (reverse < INT_MIN/10 || (reverse == INT_MIN / 10 && toAdd < -8))
+            return 0;
+        reverse = (reverse * 10) + toAdd;
+        myInt = myInt / 10;
+    }
 
-	if (reverse != 0 && isNeg)
-		reverse = -reverse;
+    if (reverse != 0 && isNeg)
+        reverse = -reverse;
 
     return reverse;
 }
@@ -32,13 +32,13 @@ reverse(long myInt)
 int
 main()
 {
-	int answer;
-	answer = reverse(321);
-	printf("%d \n", answer);
-	answer = reverse(-321);
-	printf("%d \n", answer);
-	answer = reverse(1234567905);
-	printf("%d \n", answer);
+    int answer;
+    answer = reverse(321);
+    printf("%d \n", answer);
+    answer = reverse(-321);
+    printf("%d \n", answer);
+    answer = reverse(1234567905);
+    printf("%d \n", answer);
 
-	return 0;
+    return 0;
 }
