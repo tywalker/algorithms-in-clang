b3522c7586f20681a6496d7a480250c5992ad452
tywalker <tywalker_@outlook.com>
2019-10-04 22:21:41 +0000

    accepted answer

--- leet/reverse-int.c
+++ leet/reverse-int.c
@@ -1,43 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-int reverse(long myInt)
+
+int main()
 {
-	bool isNeg = myInt < 0;
+	int *res = (int *)malloc(sizeof(int));
+	int ans = 457845887348;
 
-	if (myInt < 0) myInt = myInt * -1;
-
-	long reverse = 0;
-	long toAdd = 0;
-
-	while (myInt != 0)
-	{
-		toAdd = myInt % 10;
-		
-		printf("toAdd: %d \n", toAdd);
-		reverse = (reverse * 10) + toAdd;
-		printf("reverse: %d \n", reverse);
-		printf("myInt: %d \n", myInt);
-		myInt = myInt / 10;
-
-	}
-
-    return (isNeg) ? (reverse * -1) : reverse;
-}
-
-int
-main()
-{
-	int answer;
-	long myInt = 964632435;
-	long toAdd = 5;
-
-	long x = myInt * 10;
-	printf("%d \n", x);
-
-	answer = reverse(1534236469);
-
+	printf("%d", isOv(res, ans));
 
 	return 0;
 }
