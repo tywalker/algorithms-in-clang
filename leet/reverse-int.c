8907170431dbb239d2384ade265bc7434a1b3f2a
tywalker <tywalker_@outlook.com>
2019-10-04 22:28:00 +0000

    cleanup

--- leet/reverse-int.c
+++ leet/reverse-int.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <stdbool.h>
 #include <limits.h>
 
-int reverse(long myInt)
+int
+reverse(long myInt)
 {
     bool isNeg = myInt < 0;
 	int reverse = 0;
     int toAdd = 0;
 
-	if (isNeg) myInt = -myInt;
+	if (isNeg) 
+		myInt = -myInt;
 
 	while (myInt != 0)
 	{
 		toAdd = myInt % 10;
-        if (reverse > INT_MAX/10 || (reverse == INT_MAX / 10 && toAdd > 7)) return 0;
-        if (reverse < INT_MIN/10 || (reverse == INT_MIN / 10 && toAdd < -8)) return 0;
+        if (reverse > INT_MAX/10 || (reverse == INT_MAX / 10 && toAdd > 7))
+			return 0;
+        if (reverse < INT_MIN/10 || (reverse == INT_MIN / 10 && toAdd < -8))
+			return 0;
 		reverse = (reverse * 10) + toAdd;
 		myInt = myInt / 10;
 	}
 
 	if (reverse != 0 && isNeg)
-	{
 		reverse = -reverse;
-	}
 
     return reverse;
 }
 
-int main()
+int
+main()
 {
-	reverse(321);
+	int answer;
+	answer = reverse(321);
+	printf("%d \n", answer);
+	answer = reverse(-321);
+	printf("%d \n", answer);
+	answer = reverse(1234567905);
+	printf("%d \n", answer);
+
 	return 0;
 }
