170ef3e8257638fd365f79c14ca638e1bf423165
tywalker <tywalker_@outlook.com>
2019-10-06 00:04:59 +0000

    solution for palindrome

--- palindrome/palindrome.c
+++ palindrome/palindrome.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+long
+reverse(long x)
+{
+    long toAdd = 0;
+    long reverse = 0;
+    bool isNeg = x < 0;
+
+    while(x != 0) {
+        // 3459
+        // 345
+
+        // toAdd = 9
+        // toAdd = 5
+        toAdd = x % 10;
+
+        // 0 * 10 + 9 = 9
+        // (9 * 10) + 5 = 95
+        reverse = (reverse * 10) + toAdd;
+
+        // 345.9
+        // 34.5
+        x = x / 10;
+    }
+
+    return isNeg ? -reverse : reverse;
+}
+
+bool
+isPalindrome(long org)
+{
+    long rvs = reverse(org);
+    printf("%d %d", rvs, org);
+    return org == rvs;
+}
 
 int
 main()
 {
+    bool check = isPalindrome(200);
+
+    printf("%s \n", check ? "true" : "false");
     return 0;
 }
